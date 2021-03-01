f330f918bcca4a8228febcc31dd65a9793e0fb8f
Himanshu Hemant <himanshuhemant1512@gmail.com>
2021-03-01 18:09:37 +0530

    starting hooks

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Route,Switch, Redirect} from 'react-router-dom';
+import TempApp from './Components/TempApp';
+ 
+const App=()=>{
+  return (
+    <>
+    < TempApp />
+    </>
+  );
+};
+
+export default App;
